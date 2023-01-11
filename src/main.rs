9d71b57bd20ebbb2161ad4f60d69cff42b70c497
petar-savov <p.savov@outlook.com>
2023-01-11 23:47:51 +0000

    Include configuration reading

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
+use mailn::configuration::get_configuration;
 use mailn::startup::run;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    // Bubble up the io::Error if we failed to bind the address
-    // Otherwise call .await on our Server
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let configuration = get_configuration().expect("Failed to read config.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address).expect("Failed to bind random port");
     run(listener)?.await
 }
