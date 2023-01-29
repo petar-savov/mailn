1e732ec1d1152a2589bfce16dac38f34fc668a54
petar-savov <p.savov@outlook.com>
2023-01-29 10:05:59 +0000

    Share startup logic

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,13 @@
 use mailn::configuration::get_configuration;
-use mailn::email_client::EmailClient;
-use mailn::startup::run;
+use mailn::startup::Application;
 use mailn::telemetry::{get_subscriber, init_subscriber};
-use sqlx::postgres::PgPoolOptions;
-use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let subscriber = get_subscriber("mailn".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
-
-    let configuration = get_configuration().expect("Failed to read config.");
-    let connection_pool = PgPoolOptions::new()
-        .acquire_timeout(std::time::Duration::from_secs(2))
-        .connect_lazy_with(configuration.database.with_db());
-
-    let sender_email = configuration
-        .email_client
-        .sender()
-        .expect("Invalid sender email address.");
-    let timeout = configuration.email_client.timeout();
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url,
-        sender_email,
-        configuration.email_client.authorization_token,
-        timeout,
-    );
-
-    let address = format!(
-        "{}:{}",
-        configuration.application.host, configuration.application.port
-    );
-    let listener = TcpListener::bind(address).expect("Failed to bind random port");
-    run(listener, connection_pool, email_client)?.await
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let application = Application::build(configuration).await?;
+    application.run_until_stopped().await?;
+    Ok(())
 }
