3bc484af25a56bf96d32b4e658c07ff7295d0bb1
petar-savov <p.savov@outlook.com>
2023-02-01 07:52:20 +0000

    Bugfix: initialise email client

--- src/startup.rs
+++ src/startup.rs
@@ -78,7 +78,7 @@ pub fn run(
     email_client: EmailClient,
 ) -> Result<Server, std::io::Error> {
     let connection = web::Data::new(db_pool);
-
+    let email_client = web::Data::new(email_client);
     let server = HttpServer::new(move || {
         App::new()
             .wrap(TracingLogger::default())
