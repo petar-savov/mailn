6d204779719ae846989bf73b947e60c7ea929bc9
petar-savov <p.savov@outlook.com>
2023-02-08 08:33:36 +0000

    Don't block async execution

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -1,5 +1,6 @@
 use argon2::password_hash::SaltString;
-use argon2::{Argon2, PasswordHasher};
+use argon2::PasswordHasher;
+use argon2::{Algorithm, Argon2, Params, Version};
 use mailn::configuration::{get_configuration, DatabaseSettings};
 use mailn::startup::Application;
 use mailn::startup::{build, get_connection_pool};
@@ -66,7 +67,7 @@ pub struct TestApp {
     pub port: u16,
     pub db_pool: PgPool,
     pub email_server: MockServer,
-    test_user: TestUser,
+    pub test_user: TestUser,
 }
 
 impl TestApp {
@@ -144,10 +145,14 @@ impl TestUser {
     }
     async fn store(&self, pool: &PgPool) {
         let salt = SaltString::generate(&mut rand::thread_rng());
-        let password_hash = Argon2::default()
-            .hash_password(self.password.as_bytes(), &salt)
-            .unwrap()
-            .to_string();
+        let password_hash = Argon2::new(
+            Algorithm::Argon2id,
+            Version::V0x13,
+            Params::new(15000, 2, 1, None).unwrap(),
+        )
+        .hash_password(self.password.as_bytes(), &salt)
+        .unwrap()
+        .to_string();
         sqlx::query!(
             "INSERT INTO users (user_id, username, password_hash)
             VALUES ($1, $2, $3)",
